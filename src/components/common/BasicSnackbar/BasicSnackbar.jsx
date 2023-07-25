66d979dd674cd391d7a0effad09bede33864e880
gunnar-miklis <gunnar.miklis@gmail.com>
2023-07-25 20:19:51 +0200

    implement snackbar

---
+++ src/components/common/BasicSnackbar/BasicSnackbar.jsx
@@ -0,0 +1,27 @@
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { forwardRef } from 'react';
+
+const Alert = forwardRef( function Alert( props, ref ) {
+	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+} );
+
+export default function BasicSnackbar( { open, onClose, severity, message } ) {
+	return (
+		<>
+			<Snackbar
+				open={open}
+				autoHideDuration={6000}
+				onClose={onClose}
+			>
+				<Alert
+					onClose={onClose}
+					severity={severity}
+					sx={{ width: '100%' }}
+				>
+					{message}
+				</Alert>
+			</Snackbar>
+		</>
+	);
+}
--- src/pages/Storage/Storage.jsx
+++ src/pages/Storage/Storage.jsx
@@ -1,9 +1,38 @@
-import { Grid } from '@mui/material';
+import { useState } from 'react';
+import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
+import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
+import CommonButton from '../../components/common/CommonButton/CommonButton';
 
 export default function Storage() {
+	const [ open, setOpen ] = useState( false );
+
+	const handleClick = () => {
+		setOpen( true );
+	};
+
+	const handleClose = ( event, reason ) => {
+		if ( reason === 'clickaway' ) {
+			return;
+		}
+
+		setOpen( false );
+	};
 	return (
 		<>
-			<Grid item xs={8}></Grid>
+			<GridWrapper>
+				<CommonButton
+					variant="contained"
+					onClick={handleClick}
+				>
+					Open success snackbar
+				</CommonButton>
+				<BasicSnackbar
+					open={open}
+					onClose={handleClose}
+					severity='error'
+					message='some error'
+				/>
+			</GridWrapper>
 		</>
 	);
 }
