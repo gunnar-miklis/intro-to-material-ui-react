abf564de8150b9df054f3ab234c7cbbf88370210
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-15 14:51:17 +0200

    add handle error

--- src/components/UserTable/UserTable.jsx
+++ src/components/UserTable/UserTable.jsx
@@ -1,26 +1,29 @@
 import { useEffect, useState } from 'react';
 import DataTable from '../common/DataTable/DataTable';
+import { userTableStyles } from './styles';
 
 const columns = [
 	{ field: 'id', headerName: 'UserId', width: 150 },
 	{ field: 'name', headerName: 'Name', width: 150 },
 	{ field: 'username', headerName: 'Username', width: 150 },
-	{ field: 'email', headerName: 'E-Mail', width: 150 },
+	{ field: 'email', headerName: 'E-Mail', width: 250 },
 ];
 
-export default function UserTable() {
+export default function UserTable( { onError } ) {
 	const [ users, setUsers ] = useState( [] );
 
 	useEffect( ()=> {
 		fetch( 'https://jsonplaceholder.typicode.com/users' )
 			.then( ( response ) => response.json() )
-			.then( ( json ) => setUsers( json ) );
+			.then( ( json ) => setUsers( json ) )
+			.catch( () => onError() );
 	}, [] );
 	return (
 		<DataTable
 			rows={users}
 			columns={columns}
 			loading={!users.length}
+			sx={userTableStyles}
 		/>
 	);
 }
--- src/pages/Storage/Storage.jsx
+++ src/pages/Storage/Storage.jsx
@@ -1,11 +1,31 @@
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
 import UserTable from '../../components/UserTable/UserTable';
+import BasicCard from '../../components/common/BasicCard/BasicCard';
+import { useState } from 'react';
+import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
 
 export default function Storage() {
+	const [ open, setOpen ] = useState( false );
+
+	const handleClose = ( event, reason ) => {
+		if ( reason === 'clickaway' ) {
+			return;
+		}
+
+		setOpen( false );
+	};
 	return (
 		<>
 			<GridWrapper>
-				<UserTable />
+				<BasicCard
+					content={<UserTable onError={()=>setOpen( true )}/>}
+				/>
+				<BasicSnackbar
+					open={open}
+					onClose={handleClose}
+					severity='error'
+					message='Error fetching the API Data.'
+				/>
 			</GridWrapper>
 		</>
 	);
