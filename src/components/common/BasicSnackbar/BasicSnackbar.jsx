96510fc82fefbcf967a5aa8fdb05976ba8bcbfa4
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-15 16:08:32 +0200

    add snackbar transition

--- src/components/common/BasicSnackbar/BasicSnackbar.jsx
+++ src/components/common/BasicSnackbar/BasicSnackbar.jsx
@@ -1,26 +1,31 @@
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import { forwardRef } from 'react';
+import { Slide } from '@mui/material';
 
 const Alert = forwardRef( function Alert( props, ref ) {
 	return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 } );
 
-export default function BasicSnackbar( { open, onClose, severity, message } ) {
+export default function BasicSnackbar( { open, onClose, severity, message, hideDuration } ) {
 	return (
 		<>
 			<Snackbar
 				open={open}
-				autoHideDuration={6000}
+				autoHideDuration={hideDuration? hideDuration : 3000}
 				onClose={onClose}
+				anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
 			>
-				<Alert
-					onClose={onClose}
-					severity={severity}
-					sx={{ width: '100%' }}
-				>
-					{message}
-				</Alert>
+				<Slide direction="up" in={open} mountOnEnter unmountOnExit>
+					<Alert
+						onClose={onClose}
+						severity={severity}
+						sx={{ width: '100%' }}
+					>
+						{message}
+					</Alert>
+
+				</Slide>
 			</Snackbar>
 		</>
 	);
