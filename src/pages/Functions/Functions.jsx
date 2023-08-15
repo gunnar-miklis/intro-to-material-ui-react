69c01692ff3dcf5e117f808b3352efe4141ec1bc
gunnar-miklis <gunnar.miklis@gmail.com>
2023-08-15 14:14:41 +0200

    refactor components

--- src/pages/Functions/Functions.jsx
+++ src/pages/Functions/Functions.jsx
@@ -1,12 +1,57 @@
-import { Grid } from '@mui/material';
+import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
+import Loading from '../../components/common/Loading/Loading';
+import CommonButton from '../../components/common/CommonButton/CommonButton';
+import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
+import { useEffect, useState } from 'react';
 
 export default function Functions() {
+	const [ open, setOpen ] = useState( false );
+	const [ loading, setLoading ] = useState( true );
+
+	useEffect( () => {
+		const timer = setTimeout( () => setLoading( false ), 2000 );
+		return () => clearTimeout( timer );
+	}, [] );
+
+	const handleClick = () => {
+		setOpen( true );
+	};
+
+	const handleClose = ( event, reason ) => {
+		if ( reason === 'clickaway' ) {
+			return;
+		}
+
+		setOpen( false );
+	};
 	return (
 		<>
-			<Grid item xs={8}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius, sapien facilisis lacinia pellentesque, purus libero interdum arcu, id scelerisque erat est pretium lectus. Fusce scelerisque dictum odio, eget egestas nibh mattis ac. Suspendisse potenti. Sed porttitor sapien quis metus fermentum molestie. Ut ultrices hendrerit posuere. Nullam ipsum ex, placerat ut urna quis, rutrum mollis libero. Proin pulvinar libero eu dignissim aliquet. Quisque commodo enim a vehicula varius. Morbi vel felis et ligula lobortis maximus. Vestibulum sit amet neque et lectus blandit consectetur et a felis. Mauris lacus lorem, imperdiet ut scelerisque sed, venenatis bibendum eros.
-				Aliquam odio nulla, congue in feugiat in, vulputate sed odio. Sed quam diam, porttitor vitae auctor ut, pharetra vitae ex. Quisque id leo placerat, lacinia odio sed, tristique nulla. Duis elementum diam eget arcu laoreet porta. Quisque laoreet lorem vitae nisi porta ornare. Proin commodo, justo eu vehicula ullamcorper, est risus egestas metus, eu iaculis arcu enim eu mi. Aenean vel odio tincidunt, venenatis sapien ut, blandit arcu. In hac habitasse platea dictumst. Quisque viverra at sem sit amet aliquet. Quisque sit amet efficitur lorem, eu tincidunt felis. Ut eu feugiat tellus. Nunc blandit purus ac purus pulvinar vestibulum. Integer bibendum bibendum urna at finibus. Pellentesque rhoncus egestas tellus, nec dictum risus iaculis consectetur. Sed sollicitudin egestas nibh a varius. Donec placerat lectus id posuere lacinia.
-			</Grid>
+			<GridWrapper>
+				{ loading ?
+					<Loading>
+						<CommonButton
+							variant="contained"
+							onClick={handleClick}
+						>
+					Open success snackbar
+						</CommonButton>
+					</Loading>
+					:
+					<CommonButton
+						variant="contained"
+						onClick={handleClick}
+					>
+					Open success snackbar
+					</CommonButton>
+				}
+
+				<BasicSnackbar
+					open={open}
+					onClose={handleClose}
+					severity='error'
+					message='some error'
+				/>
+			</GridWrapper>
 		</>
 	);
 }
--- src/pages/Storage/Storage.jsx
+++ src/pages/Storage/Storage.jsx
@@ -1,56 +1,11 @@
-import { useEffect, useState } from 'react';
-import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
-import CommonButton from '../../components/common/CommonButton/CommonButton';
-import Loading from '../../components/common/Loading/Loading';
+import UserTable from '../../components/UserTable/UserTable';
 
 export default function Storage() {
-	const [ open, setOpen ] = useState( false );
-	const [ loading, setLoading ] = useState( true );
-
-	useEffect( () => {
-		const timer = setTimeout( () => setLoading( false ), 2000 );
-		return () => clearTimeout( timer );
-	}, [] );
-
-	const handleClick = () => {
-		setOpen( true );
-	};
-
-	const handleClose = ( event, reason ) => {
-		if ( reason === 'clickaway' ) {
-			return;
-		}
-
-		setOpen( false );
-	};
 	return (
 		<>
 			<GridWrapper>
-				{ loading ?
-					<Loading>
-						<CommonButton
-							variant="contained"
-							onClick={handleClick}
-						>
-					Open success snackbar
-						</CommonButton>
-					</Loading>
-					:
-					<CommonButton
-						variant="contained"
-						onClick={handleClick}
-					>
-					Open success snackbar
-					</CommonButton>
-				}
-
-				<BasicSnackbar
-					open={open}
-					onClose={handleClose}
-					severity='error'
-					message='some error'
-				/>
+				<UserTable />
 			</GridWrapper>
 		</>
 	);
